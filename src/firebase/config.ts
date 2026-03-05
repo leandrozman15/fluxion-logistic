6340ac7747b3ecc49581ae4644429d9c385557f5
leo zman <leozman15@gmail.com>
2026-03-05 23:23:49 +0000

    // Import the functions you need from the SDKs you need import { initial

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,11 +1,11 @@
 
 export const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY || '',
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN || '',
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID || '',
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET || '',
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID || '',
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID || '',
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY || 'AIzaSyBP1ejTVPBbh71XElr-CJQ7dSBYFPhnJTY',
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN || 'studio-5171832922-39b6b.firebaseapp.com',
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID || 'studio-5171832922-39b6b',
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET || 'studio-5171832922-39b6b.firebasestorage.app',
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID || '645975171719',
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID || '1:645975171719:web:6a5c8d836ada67582f4e22',
 };
 
 export const isFirebaseConfigValid = 
